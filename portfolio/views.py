a3978763f6be19ef7cca8e116b9996f33abb9b21
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-07-29 23:51:51 +0300

    css cont

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "index.html")
+    context = {
+        'thanks_to' : ['buket','cem','yagiz','muccio']
+
+    }
+    return render(request, "index.html",context)
 
 def about(request):
     return render(request,'about.html')
